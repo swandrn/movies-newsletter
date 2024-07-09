d45973444bc5191615d99afc98fd5dbf3821e3c2
swandrn <159421702+swandrn@users.noreply.github.com>
2024-07-09 16:05:51 +0200

    Function to fetch movies by page, SearchResult and Movie structs

--- src/api_handler.go
+++ src/api_handler.go
@@ -1,1 +1,59 @@
 package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type SearchResult struct {
+	Page         int     `json:"page"`
+	Results      []Movie `json:"results"`
+	TotalPages   int     `json:"total_pages"`
+	TotalResults int     `json:"total_results"`
+}
+
+type Movie struct {
+	Adult         bool   `json:"adult"`
+	Id            int    `json:"id"`
+	OriginalTitle string `json:"original_title"`
+	Title         string `json:"title"`
+	Overview      string `json:"overview"`
+	GenreId       []int  `json:"genre_ids"`
+	ReleaseDate   string `json:"release_date"`
+}
+
+func fetchMoviesByPage(pageNumber int) (SearchResult, error) {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	response, err := http.Get(fmt.Sprintf("https://api.themoviedb.org/3/discover/movie?include_adult=true&include_video=false&language=fr-FR&page=%d&sort_by=popularity.desc&api_key=%s", pageNumber, os.Getenv("TMDB_API_KEY")))
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return SearchResult{}, errors.New(response.Status)
+	}
+
+	data, err := io.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var searchResult SearchResult
+	err = json.Unmarshal(data, &searchResult)
+	if err != nil {
+		panic(err)
+	}
+
+	return searchResult, nil
+}
