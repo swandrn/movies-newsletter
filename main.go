b0342e36928f759d572ca1f7dac3154464a04b3f
swandrn <159421702+swandrn@users.noreply.github.com>
2024-07-12 21:37:13 +0200

    Serving pages and serving static files

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	tmpl "tmdb-consumer/serving_templates"
@@ -10,5 +11,12 @@ func main() {
 	http.HandleFunc("/", tmpl.HomeHandler)
 	http.HandleFunc("/home", tmpl.HomeHandler)
 	http.HandleFunc("/contact", tmpl.ContactHandler)
+	http.HandleFunc("/discover/", tmpl.DiscoverHandler)
+	http.HandleFunc("/movie/", tmpl.MovieHandler)
+	http.HandleFunc("/genre/", tmpl.GenreHandler)
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	fmt.Printf("Server started on port 8080\n")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
